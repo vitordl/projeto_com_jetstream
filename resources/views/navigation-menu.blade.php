47ec5e903aa056b26ee9ddbfc121a91d8ddbc583
Vitor na Empresa <vitortactic@gmail.com>
2023-01-04 17:42:56 -0300

    alterações

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -151,6 +151,15 @@
             </x-jet-responsive-nav-link>
         </div>
 
+        <div class="pt-2 pb-3 space-y-1">
+            <x-jet-responsive-nav-link href="{{ route('users') }}" :active="request()->routeIs('users')">
+                {{ __('Users') }}
+            </x-jet-responsive-nav-link>
+        </div>
+
+
+        
+
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="flex items-center px-4">
